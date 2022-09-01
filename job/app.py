c576b4da39ec815c1d153af45c0a713063114389
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-01 19:44:02 +0000

    file app

---
+++ job/app.py
@@ -0,0 +1,15 @@
+
+lista = [10, [3.1, 20, [30, 'baz', 2.7]], 'foo']
+
+dicionario = {'nome': 'Pedro', '01': 'Ola'}
+
+a_item = dicionario.items()
+
+tupla = ('1', '2', '3')
+
+var1 = "James" * 2 * 3
+print(var1)
+
+
+a, b, c = (1, 2, 3, 4, 5, 6, 7, 8, 9)[1::3]
+print(b)
